9bd3758f69f7bb0cacb34791c9a45ec390c12062
hyungtaek.ryu <bedmind10@gmail.com>
2021-05-03 14:01:31 +0900

    node js add express js mybatis-mapper

--- express.js
+++ express.js
@@ -7,7 +7,7 @@ const serverInfo = require('./mapper/serverInfo');
 //mybatis-mapepr 추가
 const mybatisMapper = require('mybatis-mapper');
 
-//request body의 json 객체 접근을 위해 선언.
+//request body의 json 객체 형태로 접근을 위해 선언.
 app.use(express.json());
 
 //비즈니스 로직 임포트
--- service/todoList.js
+++ service/todoList.js
@@ -2,6 +2,7 @@ module.exports = (app,dbConnection,mybatisMapper) => {
     //mapper 가져오는 경로의 root는 프로젝트 root와 같다.
     mybatisMapper.createMapper(['./react-practice-backend/mapper/todoListMapper.xml']);
 
+    const sqlFormat = {language : 'sql', indent : '  ', uppercase : true};
     app.post('/todoList/init',(req,res)=>{
         dbConnection.then(conn=>{
             conn.query(mybatisMapper.getStatement('todoListMapper', 'selectTodoList')).then(rows=>{
@@ -17,5 +18,64 @@ module.exports = (app,dbConnection,mybatisMapper) => {
                 }
             })
         })
+    });
+
+    app.post('/todoList/insert',(req,res)=>{
+        const reqData = {...req.body};
+        const mapper = mybatisMapper.getStatement('todoListMapper','insertTodoList',reqData, sqlFormat);
+
+        //getStatement 때 포맷(4번째 파라미터) 지정 시 로그에 포맷이 지정되어 찍힘.
+        console.log('query :: ',mapper);
+
+        dbConnection.then(conn=>{
+            let result = {};
+            try{
+                conn.query(mapper);
+                result.status = 'success';
+                res.send(result);
+            }catch (e){
+                result.status = 'failed';
+                result.message = e.toString();
+                res.send(result);
+            }
+        })
+    })
+
+    app.post('/todoList/update',(req,res)=>{
+        const reqData = {...req.body};
+        const mapper = mybatisMapper.getStatement('todoListMapper','updateTodoList',reqData, sqlFormat);
+        console.log('query :: ',mapper);
+
+        dbConnection.then(conn=>{
+            let result = {};
+            try{
+                conn.query(mapper);
+                result.status = 'success';
+                res.send(result);
+            }catch (e){
+                result.status = 'failed';
+                result.message = e.toString();
+                res.send(result);
+            }
+        });
+    });
+
+    app.post('/todoList/delete',(req,res)=>{
+        const reqData = {...req.body};
+        const mapper = mybatisMapper.getStatement('todoListMapper','deleteTodoList',reqData, sqlFormat);
+        console.log('query :: ',mapper);
+
+        dbConnection.then(conn=>{
+            let result = {};
+            try{
+                conn.query(mapper);
+                result.status = 'success';
+                res.send(result);
+            }catch (e){
+                result.status = 'failed';
+                result.message = e.toString();
+                res.send(result);
+            }
+        });
     });
 }
